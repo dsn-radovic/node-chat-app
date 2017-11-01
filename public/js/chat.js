3aff15e7605bbdbf641e7262f22962a305d7fff8
Dusan Radovic <d.radovic@levi9.com>
2017-11-01 15:18:06 +0100

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -76,7 +76,6 @@ $('#message-form').on('submit', function(event) {
     var messageTextbox = $('[name=message');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function(){
         messageTextbox.val('');
